7933b9d4c0821165111359d319698473619eaf36
chenmingzhen <214164051@qq.com>
2020-10-27 00:41:03 +0800

    fix:🐛 Fixed a bug where button effects could not be displayed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
+// 外部css
+// import './index.css'
 import ImageExample from '@/component/Image/example/imageExample'
+import ButtonExample from '@/component/Button/example'
+import Message from '@/component/Message'
 
 function App() {
   return (
     <>
+      <div
+        onClick={() => {
+          Message.success('Hello world')
+        }}
+      >
+        1
+      </div>
+      <ButtonExample />
       <ImageExample />
     </>
   )
