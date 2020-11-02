de582efaff4fff7c22a8cf5a390dba1b62059b04
chenmingzhen <214164051@qq.com>
2020-11-02 15:11:07 +0800

    fix:🐛 Change of judgment condition

--- src/utils/component.js
+++ src/utils/component.js
@@ -1,5 +1,4 @@
 import React from 'react'
-// import {action} from "mobx";
 
 function create(name) {
   const Base = React[name]
@@ -13,13 +12,11 @@ function create(name) {
     }
 
     setState(...args) {
-      if (this.$isMounted) super.setState(...args)
+      if (this.$isMounted !== false) super.setState(...args)
     }
 
-    // FIXME
-    // @action.bound
     forceUpdate() {
-      if (this.$isMounted) super.forceUpdate()
+      if (this.$isMounted === true) super.forceUpdate()
       if (this.$isMounted === undefined) {
         if (this.forceUpdateTimer) clearTimeout(this.forceUpdateTimer)
         this.forceUpdateTimer = setTimeout(this.forceUpdate.bind(this))
