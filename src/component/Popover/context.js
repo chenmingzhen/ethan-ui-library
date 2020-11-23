f3348ac73a5d9e07030ae54f070f0cc3970d1eaf
chenmingzhen <214164051@qq.com>
2020-11-23 23:17:41 +0800

    add Popover context event and util isDOMElement

---
+++ src/component/Popover/context.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import createReactContext from 'create-react-context'
+
+const context = createReactContext()
+
+export const { Provider } = context
+
+export const consumer = Origin => props => (
+  <context.Consumer>{value => <Origin {...props} {...value} />}</context.Consumer>
+)
