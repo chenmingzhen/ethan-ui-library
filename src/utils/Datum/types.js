e6f7c05e058f1d5df9225fdb5dd1a9297d415503
chenmingzhen <214164051@qq.com>
2020-12-07 22:51:09 +0800

    util:Datum

---
+++ src/utils/Datum/types.js
@@ -0,0 +1,23 @@
+export const changeSubscribe = name => `__CHANGE_SUBSCRIBE_${name}__`
+
+export const errorSubscribe = name => `__ERROR_SUBSCRIBE_${name}__`
+
+export const updateSubscribe = name => `__UPDATE_SUBSCRIBE_${name}__`
+
+export const CHANGE_TOPIC = '__CHANGE_TOPIC__'
+
+export const VALIDATE_TOPIC = '__VALIDATE_TOPIC__'
+
+export const RESET_TOPIC = '__RESET_TOPIC__'
+
+export const FORCE_PASS = '__VALIDATE_FORCE_PASS__'
+
+export const IGNORE_VALIDATE = '__IGNORE_VALIDATE__'
+
+export const ERROR_TYPE = '__UPDATE_ERROR_TYPE__'
+
+export const REMOVE_ERROR = '__REMOVE_ERROR_TYPE__'
+
+export const WITH_OUT_DISPATCH = '__WITH_OUT_DISPATCH__'
+
+export const IGNORE_BIND = '__IGNORE_BIND__'
