341a65df3aafc4c5f72f4c9d1a3feca29a34dfb1
chenmingzhen <214164051@qq.com>
2020-12-07 22:51:34 +0800

    util:add Rule

---
+++ src/utils/Rule/required.js
@@ -0,0 +1,20 @@
+import { deepMerge } from '@/utils/objects'
+import { substitute } from '@/utils/strings'
+import { getLocale } from '@/locale'
+
+const options = { skipUndefined: true }
+
+export const requiredMessage = props => {
+  const type = props.type === 'array' ? 'array' : 'string'
+  return substitute(getLocale(`rules.required.${type}`), props)
+}
+
+export default ({ message, tip } = {}) => msg =>
+  deepMerge(
+    {
+      required: true,
+      message: requiredMessage,
+    },
+    deepMerge({ message, tip }, { message: msg }, options),
+    options
+  )
