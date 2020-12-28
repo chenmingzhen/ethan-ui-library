bf0424de847f12826745ca9a7a515edbd18bf2b3
chenmingzhen <214164051@qq.com>
2020-12-28 22:48:40 +0800

    feat:🌲 Add Tree Component

---
+++ src/component/Tree/Checkbox.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { PureComponent } from '@/utils/component'
+import { treeClass } from '@/styles'
+import Checkbox from '../Checkbox/Checkbox'
+
+export default class extends PureComponent {
+  static propTypes = {
+    datum: PropTypes.object.isRequired,
+    disabled: PropTypes.bool,
+    id: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+  }
+
+  constructor(props) {
+    super(props)
+    this.handleChange = this.handleChange.bind(this)
+    props.datum.bind(props.id, this.forceUpdate.bind(this))
+  }
+
+  componentWillUnmount() {
+    super.componentWillUnmount()
+    this.props.datum.unbind(this.props.id)
+  }
+
+  checkDisabled() {
+    const { datum, id, disabled } = this.props
+    if (disabled) return true
+
+    return datum.isDisabled(id)
+  }
+
+  handleChange(v, checked) {
+    const { datum, id, onChange } = this.props
+    datum.set(id, checked ? 1 : 0)
+    onChange(datum.getValue(), id)
+  }
+
+  render() {
+    const { datum, id } = this.props
+    const checked = datum.getChecked(id)
+
+    return (
+      <Checkbox
+        checked={checked}
+        disabled={this.checkDisabled()}
+        onChange={this.handleChange}
+        className={treeClass('checkbox')}
+      />
+    )
+  }
+}
---
+++ src/component/Tree/index.js
@@ -0,0 +1,9 @@
+import Tree from './Tree'
+import inputable from '../Form/inputable'
+
+Tree.Select = inputable(Tree)
+Tree.Field = Tree.Select
+
+Tree.displayName = 'EthanTree'
+
+export default Tree
