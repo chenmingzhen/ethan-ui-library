be83927adef9582f6530f6160c24dc00e8ec1c05
chenmingzhen <214164051@qq.com>
2021-01-26 00:11:41 +0800

    docs

--- src/component/Slider/Container.js
+++ src/component/Slider/Container.js
@@ -11,6 +11,7 @@ const Container = props => {
   // ---------------------------------method and computed---------------------------------------
   const computedVal = useMemo(() => {
     const { range, value, scale } = props
+    // 起始值
     const from = scale[0]
     if (!range) return value || from
 
@@ -26,6 +27,7 @@ const Container = props => {
 
   const handleClick = useCallback(
     e => {
+      // 点击原点指示器不操作
       if (e.target.className.indexOf(sliderClass('indicator')) >= 0) return
       if (props.disabled) return
 
@@ -106,13 +108,14 @@ const Container = props => {
 
 Container.propTypes = {
   ...getProps(PropTypes, 'disabled', 'type'),
-  autoHide: PropTypes.bool,
-  formatScale: PropTypes.oneOfType([PropTypes.func, PropTypes.bool]),
+  range: PropTypes.bool, // 是否显示双滑块
+  autoHide: PropTypes.bool, // 是否自动隐藏当前值和刻度
+  formatScale: PropTypes.oneOfType([PropTypes.func, PropTypes.bool]), // 格式化显示刻度，为false时，不显示刻度
   height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
   onChange: PropTypes.func,
   onDrag: PropTypes.func,
-  scale: PropTypes.arrayOf(PropTypes.number),
-  step: PropTypes.number,
+  scale: PropTypes.arrayOf(PropTypes.number), // 取值范围，长度 >= 2 的数组
+  step: PropTypes.number, // 步长，必须大于等于0；为0时，只能选取 scale 指定的值
   value: PropTypes.oneOfType([PropTypes.number, PropTypes.arrayOf(PropTypes.number)]),
   vertical: PropTypes.bool,
 }
@@ -126,159 +129,3 @@ Container.defaultProps = {
 }
 
 export default memo(Container)
-
-// import React, { PureComponent } from 'react'
-// import PropTypes from 'prop-types'
-// import classnames from 'classnames'
-// import { getProps } from '@/utils/proptypes'
-// import { sliderClass } from '@/styles'
-// import Slider from './Slider'
-// import { perToValue } from './utils'
-//
-// class Container extends PureComponent {
-//   constructor(props) {
-//     super(props)
-//
-//     this.state = {}
-//
-//     this.bindElement = this.bindElement.bind(this)
-//     this.handleClick = this.handleClick.bind(this)
-//     this.handleChange = this.handleChange.bind(this)
-//     this.handleFirstDrag = this.handleDrag.bind(this, 0)
-//     this.handleSecondDrag = this.handleDrag.bind(this, 1)
-//   }
-//
-//   getValue() {
-//     const { range, value, scale } = this.props
-//     const from = scale[0]
-//     if (!range) return value || from
-//
-//     let val = value
-//     if (range && !Array.isArray(value)) val = [from, from]
-//     if (val[0] > val[1]) val = [val[1], val[0]]
-//     return val
-//   }
-//
-//   bindElement(el) {
-//     this.innerElement = el
-//   }
-//
-//   handleClick(e) {
-//     if (e.target.className.indexOf(sliderClass('indicator')) >= 0) return
-//     if (this.props.disabled) return
-//
-//     const { scale, step, vertical, range } = this.props
-//     const rect = this.innerElement.getBoundingClientRect()
-//     const per = vertical ? 1 - (e.clientY - rect.top) / rect.height : (e.clientX - rect.left) / rect.width
-//
-//     const val = perToValue(per, scale, step)
-//
-//     if (!range) {
-//       this.props.onChange(val)
-//       return
-//     }
-//
-//     const value = [...this.getValue()]
-//     if (val < value[0]) value[0] = val
-//     else value[1] = val
-//
-//     this.props.onChange(value)
-//   }
-//
-//   handleChange(index, val) {
-//     const { range } = this.props
-//     if (!range) {
-//       this.props.onChange(val)
-//       return
-//     }
-//
-//     const value = [...this.getValue()]
-//     value[index] = val
-//     this.props.onChange(value)
-//   }
-//
-//   handleDrag(index, value) {
-//     const { range } = this.props
-//     if (!range) this.props.onDrag(value)
-//   }
-//
-//   renderScale() {
-//     const { autoHide, formatScale, scale } = this.props
-//     if (!formatScale) return null
-//
-//     return (
-//       <div className={sliderClass('scale', !autoHide && 'show')}>
-//         {scale.map((s, i) => (
-//           <div key={s}>
-//             <span>{formatScale(s, i)}</span>
-//           </div>
-//         ))}
-//       </div>
-//     )
-//   }
-//
-//   render() {
-//     const { range, height, style, vertical, ...other } = this.props
-//     const className = classnames(
-//       sliderClass('_', vertical && 'vertical', this.props.disabled && 'disabled'),
-//       this.props.className
-//     )
-//
-//     let value = this.getValue()
-//     if (!Array.isArray(value)) value = [0, value]
-//
-//     let newStyle = style
-//     if (vertical) newStyle = Object.assign({ height }, style)
-//
-//     return (
-//       <div style={newStyle} className={className}>
-//         <div className={sliderClass('background')} />
-//         <div ref={this.bindElement} onClick={this.handleClick} className={sliderClass('inner')}>
-//           {range && (
-//             <Slider
-//               {...other}
-//               index={0}
-//               max={value[1]}
-//               onChange={this.handleChange}
-//               value={value[0]}
-//               vertical={vertical}
-//             />
-//           )}
-//
-//           <Slider
-//             {...other}
-//             index={1}
-//             min={value[0]}
-//             onChange={this.handleChange}
-//             value={value[1]}
-//             vertical={vertical}
-//           />
-//         </div>
-//         {this.renderScale()}
-//       </div>
-//     )
-//   }
-// }
-//
-// Container.propTypes = {
-//   ...getProps(PropTypes, 'disabled', 'type'),
-//   autoHide: PropTypes.bool,
-//   formatScale: PropTypes.oneOfType([PropTypes.func, PropTypes.bool]),
-//   height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-//   onChange: PropTypes.func,
-//   onDrag: PropTypes.func,
-//   scale: PropTypes.arrayOf(PropTypes.number),
-//   step: PropTypes.number,
-//   value: PropTypes.oneOfType([PropTypes.number, PropTypes.arrayOf(PropTypes.number)]),
-//   vertical: PropTypes.bool,
-// }
-//
-// Container.defaultProps = {
-//   height: 200,
-//   scale: [0, 100],
-//   step: 1,
-//   vertical: false,
-//   formatScale: v => v,
-// }
-//
-// export default Container
--- src/component/Slider/utils/index.js
+++ src/component/Slider/utils/index.js
@@ -16,16 +16,32 @@ export function valueToPer(value, scale) {
   return (ps + (value - min) / (max - min)) / range
 }
 
+/**
+ * 百分比 => 真实值
+ * @param per 百分比
+ * @param scale 范围
+ * @param step 步长
+ * @returns {*}
+ */
 export function perToValue(per, scale, step = 1) {
+  // ❗❗❗ 范围值数组长度-1
   const range = scale.length - 1
 
   if (step === 0) return scale[Math.round(per * range)]
 
   if (per >= 1) return scale[range]
+
+  // 整数百分比值转化成整数值
   const ps = Math.floor(per * range)
+
+  // 下限
   const min = scale[ps]
+
+  // 上限
   const max = scale[ps + 1]
+  // max-min之间的数量值
   const count = (max - min) / step
+  //  特征值周期
   const sper = (per - ps / range) * range
 
   return toPrecision(min + Math.round(sper * count) * step)
