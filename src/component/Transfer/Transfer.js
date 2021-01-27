b7f8975224453c79b7cfe0b7fdef9c2273e67b6d
chenmingzhen <214164051@qq.com>
2021-01-27 12:45:36 +0800

    transfer context

---
+++ src/component/Transfer/Transfer.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import PropsTypes from 'prop-types'
+import classnames from 'classnames'
+import Btns from './btns'
---
+++ src/component/Transfer/context.js
@@ -0,0 +1,3 @@
+import createContext from 'create-react-context'
+
+export const { Provider, Consumer } = createContext()
