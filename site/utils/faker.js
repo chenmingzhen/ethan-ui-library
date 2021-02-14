122a20f71f1f3741da5fe9741372e26b8910164b
chenmingzhen <214164051@qq.com>
2021-02-14 18:24:58 +0800

    remove faker.js

--- site/utils/faker.js
+++
@@ -1,7 +0,0 @@
-export function pickNumber(max = 65555, min = 0, fixed = 2) {
-  if (typeof max === 'string') max = parseInt(max, 10)
-  if (typeof min === 'string') min = parseInt(min, 10)
-
-  const num = Math.random() * (max - min) + min
-  return parseFloat(num.toFixed(fixed), 0)
-}
