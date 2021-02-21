22b39710486a06f7a1472952d4e6c2fc93aa04eb
chenmingzhen <214164051@qq.com>
2021-02-21 14:51:07 +0800

    fix:🐛 src-index scripts

--- scripts/src-index.js
+++ scripts/src-index.js
@@ -4,22 +4,27 @@ const ejs = require('ejs')
 const pack = require('../package')
 
 const rootPath = path.resolve(__dirname, '../src')
+
+const componentsPath = path.join(rootPath, '/component')
+
 const files = fs
-  .readdirSync(rootPath)
-  .filter(n => fs.lstatSync(path.resolve(rootPath, n)).isDirectory() && /^[A-Z]/.test(n))
-  .filter(v => v !== 'List' && v !== 'DataList')
+  .readdirSync(componentsPath)
+  // lstat获取文件信息（不解析符号链接）。
+  .filter(n => fs.lstatSync(path.resolve(componentsPath, n)).isDirectory() && /^[A-Z]/.test(n))
+  // TODO finish form and table then remove de filter for them
+  .filter(v => v !== 'List' && v !== 'DataList' && v !== 'Form' && v !== 'Table')
 
 const line = `// Created by scripts/src-index.js.
 import './styles/normalize.less'
 
 export { setLocale } from './locale'
-// export { color, style } from './utils/expose'
 export { default as config, setConfig } from './config'
 
-// export { default as LazyList } from './List/LazyList'
-// export { default as List } from './DataList'
+export {FontAwesome} from './component/Icon'
+export { default as Lazyload } from './component/LazyLoad'
+
 <% files.forEach(function (name) { -%>
-export { default as <%= name %> } from './<%= name %>'
+export { default as <%= name %> } from './component/<%= name %>'
 <% }) -%>`
 
 const text = ejs.render(line, { files, version: pack.version })
