72e5a03653885da15c9bc0dd40535e617ed494a4
chenmingzhen <214164051@qq.com>
2021-02-21 15:58:01 +0800

    update links computed rules

--- src/component/Pagination/Links.js
+++ src/component/Pagination/Links.js
@@ -15,47 +15,41 @@ const Links = props => {
     const tMax = Math.ceil(total / pageSize)
     const tLinks = []
 
-    let right
-    // 页码左部分
+    // 一行除去左右指示 包含<< >> 一共九个links
+    // 左边存在<<的情况 (包含<<指示)最多2个值 所以left最大为3 最小为1
+
+    // 计算阶段
     let left = current - Math.floor(span / 2)
-    if (left < 3) {
-      left = 3
-    }
-    // 页码右部分
+
+    let right
+
+    if (left < 3) left = 3
+
     right = left + span
 
-    if (right + 1 >= tMax) {
-      right = tMax - 1
+    if (right >= tMax) {
+      right = tMax
       left = right - span
-      if (left < 1) {
-        left = 1
-      }
-    } else {
-      right -= left > 1 ? 1 : 0
+
+      if (left <= 0) left = 1
     }
 
-    if (left > 1) {
-      tLinks.push(1)
-    }
-    if (left === 3) {
-      tLinks.push(2)
-    } else if (left > 3) {
-      tLinks.push('<<')
-    }
+    // push 阶段
+    if (left > 1) tLinks.push(1)
 
-    for (let i = left; i < right + 1; i++) {
+    if (left === 3) tLinks.push(2)
+
+    if (left > 3) tLinks.push('<<')
+
+    for (let i = left; i < right; i++) {
       tLinks.push(i)
     }
 
-    if (right === tMax - 2) {
-      tLinks.push(tMax - 1)
-    } else if (right < tMax - 1) {
+    if (right < tMax) {
       tLinks.push('>>')
     }
 
-    if (right < tMax) {
-      tLinks.push(tMax)
-    }
+    tLinks.push(tMax)
 
     return { links: tLinks, max: tMax }
   }, [current, total, pageSize, span])
