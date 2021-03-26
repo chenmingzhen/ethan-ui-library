f2cb30125e4f6aeea23f7a5b88f91bddf28b97cc
chenmingzhen <214164051@qq.com>
2021-03-26 23:16:39 +0800

    add group docs

--- src/component/Select/group.js
+++ src/component/Select/group.js
@@ -19,6 +19,7 @@ export default Origin =>
         data: [],
       }
       this.groupByData = this.groupByData.bind(this)
+      // groupKey往List Option中传递 判别是否为组名
       this.groupKey = getUidStr()
     }
 
@@ -26,6 +27,7 @@ export default Origin =>
       this.groupByData()
     }
 
+    // 数据源更新后 重新分组
     componentDidUpdate(prevProps) {
       if (prevProps.data !== this.props.data) this.groupByData()
     }
@@ -46,6 +48,16 @@ export default Origin =>
         groupData[g].push(d)
       })
 
+      // {"City":[{"kmqcy6mw":"City"},{"value":"Beijing","tag":"1"},{"value":"Shanghai","tag":"1"}],"Country":[{"kmqcy6mw":"Country"},{"value":"China","tag":"2"}],"Other":[{"kmqcy6mw":"Other"},{"value":"Mars","tag":"3"}]}
+      // console.log(groupData)
+
+      //  0: {kmqcy6ol: "City"}
+      // 1: {value: "Beijing", tag: "1"}
+      // 2: {value: "Shanghai", tag: "1"}
+      // 3: {kmqcy6ol: "Country"}
+      // 4: {value: "China", tag: "2"}
+      // 5: {kmqcy6ol: "Other"}
+      // 6: {value: "Mars", tag: "3"}
       this.setState({
         data: Object.keys(groupData).reduce((p, v) => (v ? p.concat(groupData[v]) : groupData[v].concat(p)), []),
       })
