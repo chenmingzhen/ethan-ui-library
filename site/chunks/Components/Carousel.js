d84e4ad051f2b25c19edd2a954f0a83a1cebe292
chenmingzhen <214164051@qq.com>
2021-04-01 23:26:04 +0800

    add example

--- site/chunks/Components/Carousel.js
+++ site/chunks/Components/Carousel.js
@@ -14,6 +14,16 @@ const source = locate(cn, en)
 
 const examples = [
     {
+    name: '1-base',
+    title: locate(
+    '基本用法 \n 轮播组件提供了三种动画过渡方式，可以切换选项查看效果',
+    'Base \n The carousel component provides three modes of animation transition. Change the option to view the result.'
+    ),
+    component: require('doc/pages/components/Carousel/example-1-base.js').default,
+    // webpack静态资源内联raw-loader, Example中使用，代码内容从此处通过正则筛选
+    rawText: require('!raw-loader!doc/pages/components/Carousel/example-1-base.js'),
+    },
+    {
     name: '2-custom-indicator',
     title: locate(
     '自定义 Indicator \n 当 indicatorType 为函数时，可以自定义 Indicator',
