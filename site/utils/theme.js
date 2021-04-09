2e35e0607b35485532d9b2c42a3361808e9a61be
chenmingzhen <214164051@qq.com>
2021-04-09 13:18:10 +0800

    fix error theme search

--- site/utils/theme.js
+++ site/utils/theme.js
@@ -1,15 +1,29 @@
+import Theme from '../enum/themes'
+
 let theme = 'default'
 let link
 
-export const THEMES = ['default', 'antd']
-
 const getParameterByName = name => {
   const { search } = window.location
-  const regex = new RegExp(`[?&]${name}(=([^&#]*)|&|#|$)`)
+
+  // [?&] 代表只要匹配到其中一个即可,name=为固定匹配 后面的匹配规则为=号后面不能是&或#开头 然后后面任意字符
+  const regex = new RegExp(`[?&]${name}(=([^&#]*))`)
   const results = regex.exec(search)
 
+  /* 
+    results
+    0: "?theme=default"
+    1: "=default"
+    2: "default"
+    groups: undefined
+    index: 0
+    input: "?theme=default"
+    length: 3
+  */
   if (!results) return null
-  if (!results[2]) return ''
+
+  // 不存在 或者主题名称不匹配 默认返回default
+  if (!results[2] || !Theme.map(({ content }) => content).some(it => it === results[2])) return ''
   return decodeURIComponent(results[2].replace(/\+/g, ' '))
 }
 
