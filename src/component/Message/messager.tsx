ed129823122873ef36a65bed734476c117ceb9c6
chenmingzhen <214164051@qq.com>
2021-05-30 16:39:09 +0800

    refactor:👍use Map to store el and container

--- src/component/Message/messager.tsx
+++ src/component/Message/messager.tsx
@@ -4,9 +4,9 @@ import { messageClass } from '@/styles'
 import Container from './Container'
 
 // 存放同一position类型的div容器
-const elements = {}
+const elements = new Map<string, HTMLDivElement>()
 // 存放同一position类型的组件容器
-const components = {}
+const components = new Map<string, Container>()
 
 /*
   type 为 position
@@ -14,34 +14,47 @@ const components = {}
 */
 function getElement(type) {
     const div = document.createElement('div')
+
     div.className = messageClass('_', type)
 
     document.body.appendChild(div)
-    elements[type] = div
+
+    elements.set(type, div)
+
     return div
 }
 
 export function destroy(type) {
     // 卸载组件 装组件的容器
-    if (elements[type]) {
-        ReactDOM.unmountComponentAtNode(elements[type])
-        document.body.removeChild(elements[type])
-        delete elements[type]
+    if (elements.has(type)) {
+        const element = elements.get(type)
+        ReactDOM.unmountComponentAtNode(element)
+        document.body.removeChild(element)
+
+        elements.delete(type)
     }
-    if (components[type]) {
-        delete components[type]
+
+    if (components.has(type)) {
+        components.delete(type)
     }
 }
 
 export function closeWithAnimation(type) {
-    for (type in components) {
-        if (components[type]) components[type].removeAllMessage()
+    if (type) {
+        const container = components.get(type)
+
+        if (container) container.removeAllMessage()
+    } else {
+        components.forEach(container => {
+            container.removeAllMessage()
+        })
     }
 }
 
 export function getComponent(type): Promise<Container> {
     return new Promise(resolve => {
-        const component = components[type]
+        const component = components.get(type)
+
         // 判断有无这个type(position)的容器  每个type对应一个所有组件容器
         if (component) {
             resolve(component)
@@ -51,7 +64,7 @@ export function getComponent(type): Promise<Container> {
                 <Container
                     /* resolve这个实例回去 并记录在组件容器中 */
                     ref={comp => {
-                        components[type] = comp
+                        components.set(type, comp)
                         resolve(comp)
                     }}
                     onDestroy={destroy.bind(null, type)}
