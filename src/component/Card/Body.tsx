194cfaa28ed84d360b9720c75792eba61c51b8c0
chenmingzhen <214164051@qq.com>
2021-06-08 23:40:58 +0800

    remove unuseful import

--- src/component/Card/Body.tsx
+++ src/component/Card/Body.tsx
@@ -1,5 +1,4 @@
-import React, { PureComponent } from 'react'
-import PropTypes from 'prop-types'
+import React from 'react'
 import classnames from 'classnames'
 import { cardClass } from '@/styles'
 import List from '../List'
