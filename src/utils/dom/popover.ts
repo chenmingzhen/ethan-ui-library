9822773cc2959c456c1a45902ce62d3f00eae25c
chenmingzhen <214164051@qq.com>
2021-07-13 23:08:40 +0800

    docs: getPosition

--- src/utils/dom/popover.ts
+++ src/utils/dom/popover.ts
@@ -1,17 +1,23 @@
-// @ts-nocheck
 import { docScroll, docSize } from '@/utils/dom/document'
 
 const posKeys = ['left', 'top', 'bottom', 'right']
 
 export const getPosition = (position, el, container = document.body) => {
     const rect = el.getBoundingClientRect()
+
     let containerRect = { top: 0, left: 0, bottom: 0, right: 0 }
+
     if (container.tagName === 'BODY') container = undefined
+
     if (container) containerRect = container.getBoundingClientRect()
+
+    // 如果container是body 则滚动参与计算
+    // 反之 只需要el和实际容器的 BoundingClientRect即可推出计算
     const scrollTop = container ? 0 : docScroll.top
     const scrollLeft = container ? 0 : docScroll.left
 
-    const pos = {}
+    const pos: { left?: number; top?: number; right?: number } = {}
+
     switch (position) {
         // windows滚动的距离+元素到屏幕的距离-元素容器到屏幕的距离
         case 'top-left':
