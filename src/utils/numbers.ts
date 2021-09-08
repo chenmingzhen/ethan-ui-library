6165242dd0142b6762494534233bd00381de1e41
chenmingzhen <214164051@qq.com>
2021-09-08 23:34:16 +0800

    util: numbers

--- src/utils/numbers.ts
+++ src/utils/numbers.ts
@@ -1,14 +1,9 @@
-// @ts-nocheck
-// Array.from({length:2},(v,k)=>k+1)
-// [1,2]
-export function range(end, start = 0) {
-    const delta = end - start
-    if (typeof delta !== 'number' || Number.isNaN(delta)) {
-        console.error(new Error('end can not computed with start'))
-    }
+/** Array.from({length:2},(v,k)=>k+1) */
+/** [1,2] */
+export function range(end: number, start = 0) {
     return Array.from({ length: end - start }, (v, k) => k + start)
 }
 
-export function toPrecision(num, precision = 12) {
+export function toPrecision(num: number, precision = 12) {
     return +parseFloat(num.toPrecision(precision))
 }
