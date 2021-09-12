c4575c79f8fcfd8ef893db35563f574d9e7c8ba2
chenmingzhen <214164051@qq.com>
2021-09-12 10:03:45 +0800

    fix: Button type fix

--- src/component/Button/index.ts
+++ src/component/Button/index.ts
@@ -4,6 +4,8 @@ import Group, { ButtonGroupProps } from './Group'
 
 export interface ButtonComponent extends MemoExoticComponent<React.FC<ButtonProps>> {
     Group: typeof Group
+
+    IS_ETHAN_BUTTON: true
 }
 
 export { ButtonProps, ButtonGroupProps }
@@ -11,5 +13,7 @@ export { ButtonProps, ButtonGroupProps }
 const ComputedButton = Button as ButtonComponent
 
 ComputedButton.Group = Group
+
+ComputedButton.IS_ETHAN_BUTTON = true
 
 export default ComputedButton
