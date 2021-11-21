bd07ded0fa12a172e6db6723eee094ed3014e961
chenmingzhen <214164051@qq.com>
2021-11-21 18:48:57 +0800

    home less fix

--- site/styles/index.ts
+++ site/styles/index.ts
@@ -1,8 +1,14 @@
-import classGenerate from '../utils/classname.ts'
+import classGenerate from '../utils/classname'
+import * as exampleLess from './example.less'
+import * as homeLess from './home.less'
+import * as mainLess from './index.less'
+import * as markDownLess from './markdown.less'
+import * as navLess from './nav.less'
+import * as headerless from './header.less'
 
-export const exampleClass = classGenerate(require('./example.less'), 'example')
-export const headerClass = classGenerate(require('./header.less'), 'header')
-export const homeClass = classGenerate(require('./home.less'), 'home')
-export const mainClass = classGenerate(require('./index.less'), 'main')
-export const markdownClass = classGenerate(require('./markdown.less'), 'markdown')
-export const navClass = classGenerate(require('./nav.less'), 'nav')
+export const exampleClass = classGenerate(exampleLess, 'example')
+export const headerClass = classGenerate(headerless, 'header')
+export const homeClass = classGenerate(homeLess, 'home')
+export const mainClass = classGenerate(mainLess, 'main')
+export const markdownClass = classGenerate(markDownLess, 'markdown')
+export const navClass = classGenerate(navLess, 'nav')
