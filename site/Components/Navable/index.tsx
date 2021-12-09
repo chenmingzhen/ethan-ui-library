f14c6ab8bcfabb7e2ae0c360f54d9a62d4ba3288
chenmingzhen <214164051@qq.com>
2021-12-09 19:12:05 +0800

    feat:Navable

--- site/Components/Navable/index.tsx
+++ site/Components/Navable/index.tsx
@@ -1,95 +1,103 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Sticky } from 'ethan/index'
-import { useUpdate } from 'react-use'
+import { useLocation } from 'react-use'
 import { navClass } from 'doc/styles'
+import { Heading } from 'doc/type'
 import history from '../history'
 
 const scrollTo = id => {
-  const isSingleMode = history.location.search.indexOf('?example=') === 0
-  if (isSingleMode) {
-    history.push(`${history.location.pathname}?example=${id.replace('heading-', '')}`)
-  } else {
-    history.push(`${history.location.pathname}#${id}`)
-    const element = document.getElementById(id)
-    if (element) element.scrollIntoView()
-  }
+    const isSingleMode = history.location.search.indexOf('?example=') === 0
+
+    if (isSingleMode) {
+        history.push(`${history.location.pathname}?example=${id.replace('heading-', '')}`)
+    } else {
+        history.push(`${history.location.pathname}#${id}`)
+
+        const element = document.getElementById(id)
+
+        element?.scrollIntoView()
+    }
 }
 
-// 右侧导航条
-export default function(Component) {
-  return function Nav(prop) {
-    // 当前active的header
-    const [active, setActive] = useState('')
-    // h标签array
-    const [headings] = useState([])
-    // 路由hash
-    const { hash } = prop.location
-
-    const update = useUpdate()
-
-    const setHeading = useCallback(hs => {
-      hs.forEach(h => {
-        headings.push(h)
-      })
-      update()
-    }, [])
-
-    const hashScroll = useCallback(() => {
-      if (hash) {
-        const element = document.querySelector(hash)
-        if (element)
-          setTimeout(() => {
-            element.scrollIntoView()
-          }, 50)
-      }
-    }, [hash])
-
-    useEffect(() => {
-      hashScroll()
-      const handleScroll = () => {
-        const top = document.documentElement.scrollTop
-        const hs = headings.filter(h => h.level === 3 && h.children[0])
-        if (hs.length === 0) return
-
-        let newActive = hs[0].id
-        hs.forEach(h => {
-          const el = document.querySelector(`#${h.id}`)
-          if (!el) return
-          if (el.offsetTop <= top) newActive = h.id
-        })
-        setActive(newActive)
-      }
-
-      document.addEventListener('scroll', handleScroll)
-      handleScroll()
-      return () => {
-        document.removeEventListener('scroll', handleScroll)
-      }
-    }, [])
-
-    const renderNav = () => (
-      <Sticky className={navClass('sticky')} top={50}>
-        <div className={navClass('nav')}>
-          {headings.map((h, i) => {
-            const children = h.children.filter(c => typeof c === 'string')
-            return (
-              <a
-                key={i}
-                className={navClass(`level-${h.level}`, active === h.id && 'active')}
-                onClick={scrollTo.bind(null, h.id)}
-              >
-                {children}
-              </a>
-            )
-          })}
-        </div>
-      </Sticky>
-    )
-    return (
-      <div className={navClass('_')}>
-        <Component onHeadingSetted={setHeading} />
-        {!prop.noNav && renderNav()}
-      </div>
-    )
-  }
+interface NavProps {
+    noNav?: boolean
 }
+
+const Navable = (Component: React.ComponentType<{ onHeadingSet: (heading: string[]) => void }>) => {
+    const Nav: React.FC<NavProps> = ({ noNav }) => {
+        const [active, setActive] = useState('')
+
+        const [headings, setHeadings] = useState<Heading[]>([])
+
+        const location = useLocation()
+
+        const { hash } = location
+
+        useEffect(() => {
+            function handleScroll() {
+                const top = document.documentElement.scrollTop
+
+                const hasChildrenHeading = headings.filter(heading => heading.level === 3 && heading.children.length)
+
+                if (hasChildrenHeading.length === 0) return
+
+                let newActive = hasChildrenHeading[0].id
+
+                hasChildrenHeading.forEach(h => {
+                    const el = document.querySelector(`#${h.id}`) as HTMLElement
+
+                    if (el?.offsetTop <= top) newActive = h.id
+                })
+
+                setActive(newActive)
+            }
+
+            handleScroll()
+
+            if (hash) {
+                const element = document.querySelector(hash)
+
+                setTimeout(() => {
+                    element?.scrollIntoView()
+                }, 20)
+            }
+
+            document.addEventListener('scroll', handleScroll)
+
+            return () => {
+                document.removeEventListener('scroll', handleScroll)
+            }
+        }, [headings])
+
+        const renderNav = () => (
+            <Sticky className={navClass('sticky')} top={50}>
+                <div className={navClass('nav')}>
+                    {headings.map((heading, index) => {
+                        const children = heading.children.filter(c => typeof c === 'string')
+
+                        return (
+                            <a
+                                key={index}
+                                className={navClass(`level-${heading.level}`, active === heading.id && 'active')}
+                                onClick={scrollTo.bind(null, heading.id)}
+                            >
+                                {children}
+                            </a>
+                        )
+                    })}
+                </div>
+            </Sticky>
+        )
+        return (
+            <div className={navClass('_')}>
+                <Component onHeadingSet={setHeadings} />
+
+                {!noNav && renderNav()}
+            </div>
+        )
+    }
+
+    return React.memo(Nav)
+}
+
+export default Navable
