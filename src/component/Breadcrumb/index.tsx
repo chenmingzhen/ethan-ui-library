b989447ff78a46c260338f578f0e6d879b59e032
chenmingzhen <214164051@qq.com>
2021-12-28 13:02:29 +0800

    refactor:Breadcrumb refactor

--- src/component/Breadcrumb/index.tsx
+++ src/component/Breadcrumb/index.tsx
@@ -1,50 +1,51 @@
-// @ts-nocheck
-import React, { useCallback, memo } from 'react'
-import PropTypes from 'prop-types'
+import React, { useCallback } from 'react'
 import classnames from 'classnames'
-import { getProps, defaultProps } from '@/utils/proptypes'
+import { defaultProps } from '@/utils/proptypes'
 import { breadcrumbClass } from '@/styles'
-import { getKey } from '@/utils/uid'
 import Popover from '../Popover'
 import Caret from '../icons/Caret'
 
-const Breadcrumb = props => {
-    const { data, separator, keygen } = props
+export interface BreadcrumbData {
+    onClick?(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>)
+
+    url?: string
+
+    icon?: React.ReactNode
+
+    title?: React.ReactNode
+}
+
+export interface BreadcrumbProps {
+    separator?: string
+
+    data: BreadcrumbData[]
+
+    className?: string
+
+    renderItem?(item: BreadcrumbData): React.ReactNode
+
+    style?: React.CSSProperties
+}
+
+const Breadcrumb: React.FC<BreadcrumbProps> = props => {
+    const { separator, data } = props
     const className = classnames(breadcrumbClass('_'), props.className)
 
-    const renderArray = useCallback(
-        arrayData => {
-            const first = arrayData[0]
-
-            return (
-                <span>
-                    {renderItem(first)}
-                    <span className={breadcrumbClass('down')}>
-                        <Caret />
-                    </span>
-                    <Popover position="bottom">
-                        {arrayData.slice(1).map((d, i) => (
-                            <div className={breadcrumbClass('dropdown-item')} key={i}>
-                                {renderItem(d)}
-                            </div>
-                        ))}
-                    </Popover>
-                </span>
-            )
-        },
-        [data]
-    )
-
     const renderItem = useCallback(
-        d => {
-            const { renderItem: propsRenderItem } = props
+        (d: BreadcrumbData) => {
             let item = d.title
+
             if (!React.isValidElement(item)) {
                 if (d.onClick || d.url) {
-                    const newProps = {
+                    const newProps: React.DetailedHTMLProps<
+                        React.AnchorHTMLAttributes<HTMLAnchorElement>,
+                        HTMLAnchorElement
+                    > = {
                         onClick: d.onClick,
                     }
+
                     if (d.url) newProps.href = d.url
+
                     item = (
                         <a {...newProps}>
                             {d.icon}
@@ -57,15 +58,47 @@ const Breadcrumb = props => {
                     item = <b>{d.title}</b>
                 }
             }
-            return propsRenderItem ? propsRenderItem(d) : item
+
+            return props.renderItem?.(d) ?? item
         },
-        [data, props.renderItem]
+        [props.renderItem]
+    )
+
+    const renderArray = useCallback(
+        (arrayData: BreadcrumbData[]) => {
+            const first = arrayData[0]
+
+            return (
+                <Popover
+                    placement="bottom"
+                    className={breadcrumbClass('popover')}
+                    content={hide => (
+                        <>
+                            {arrayData.slice(1).map((d, i) => (
+                                <span className={breadcrumbClass('dropdown-item')} key={i} onClick={hide.bind(this)}>
+                                    {renderItem(d)}
+                                </span>
+                            ))}
+                        </>
+                    )}
+                >
+                    <span>
+                        <span>{renderItem(first)}</span>
+
+                        <span className={breadcrumbClass('down')}>
+                            <Caret />
+                        </span>
+                    </span>
+                </Popover>
+            )
+        },
+        [renderItem]
     )
 
     return (
         <div className={className} style={props.style}>
             {data.map((d, index) => (
-                <span key={keygen ? getKey(d, keygen, index) : index}>
+                <span key={`__BREADCRUMB__ITEM__${index}`}>
                     {Array.isArray(d) ? renderArray(d) : renderItem(d)}
                     {index !== data.length - 1 ? (
                         <span className={breadcrumbClass('separator')}>{separator}</span>
@@ -76,13 +109,6 @@ const Breadcrumb = props => {
     )
 }
 
-Breadcrumb.protoTypes = {
-    ...getProps(PropTypes),
-    data: PropTypes.array,
-    renderItem: PropTypes.func,
-    separator: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
-}
-
 Breadcrumb.defaultProps = {
     ...defaultProps,
     data: [],
@@ -91,4 +117,4 @@ Breadcrumb.defaultProps = {
 
 Breadcrumb.displayName = 'EthanBreadcrumb'
 
-export default memo(Breadcrumb)
+export default React.memo(Breadcrumb)
