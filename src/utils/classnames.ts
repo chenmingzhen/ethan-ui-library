3eb1c1091e407699a63b38efd2bf12e1c3b7cbf5
chenmingzhen <214164051@qq.com>
2021-12-31 18:43:28 +0800

    remove annotation

--- src/utils/classnames.ts
+++ src/utils/classnames.ts
@@ -1,24 +1,18 @@
 import classnames from 'classnames'
 import config from '../config'
 
-/*
-    create a new className generate function, add namespace, handle css module
- * @param style - object; for css module  eg:if import from less and without cssModule, style is {}
- * @param module - string    eg:"message"
- * @param prefix - string, default value is 'so'
- */
 export default (style, module, prefix = config.prefix) => (...args) => {
     const className = classnames(...args)
+
     if (!className) return ''
 
     const ns = `${prefix}${module ? `-${module}` : '-'}`
 
-    // _ means default(ns)
     let list = className.split(' ').map(item => (item === '_' ? ns : `${ns}-${item}`))
 
-    // If you turn on modularity
     if (config.cssModule) {
         list = list.map(item => style[item] || item)
     }
+
     return list.join(' ')
 }
