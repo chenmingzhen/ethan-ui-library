2b192d27838b3272dc9e5258af742ac9412017af
chenmingzhen <214164051@qq.com>
2022-01-24 13:15:03 +0800

    add rule type

--- src/component/Rule/index.ts
+++ src/component/Rule/index.ts
@@ -1,42 +1,9 @@
-import { deepMerge } from '@/utils/objects'
-import { substitute } from '@/utils/strings'
-import { getLocale } from '@/locale'
-import { mergeOptions } from './util'
-import { Options, RequiredOptions } from './type/index'
+import { BaseOptions, Validator, BaseOptionKeys, BaseOptionRuleOutput } from './type/index'
 
-export const RULE_TYPE = 'RULE_TYPE'
+/**
+ * @see https://stackoverflow.com/questions/70815177/typescript-complex-type-inference/70827227#70827227
+ */
 
-export default class Rule {
-    options: Options
-
-    $$type = RULE_TYPE
-
-    constructor(propOptionArray: Options[] = []) {
-        this.options = mergeOptions({}, ...propOptionArray)
-
-        Reflect.defineProperty(this.required, 'isInnerValidator', { value: true })
-        Reflect.defineProperty(this.validator, 'isInnerValidator', { value: true })
-    }
-
-    required = (msg: string): { required: boolean; message: string; tip?: string } => {
-        const deepMergeOptions = { skipUndefined: true }
-
-        const { message, tip } = (this.options.required as RequiredOptions) || {}
-
-        return deepMerge(
-            {
-                required: true,
-                message: props => {
-                    // 如果props为空 则使用内置的requiredMessage 文案
-                    return substitute(getLocale(`rules.required.${props.type === 'array' ? 'array' : 'string'}`), props)
-                },
-            },
-            deepMerge({ message, tip }, { message: msg }, deepMergeOptions),
-            deepMergeOptions
-        )
-    }
-
-    validator = () => ({
-        validator: this.options.validator,
-    })
+function Rule<R extends Validator | BaseOptions>(options: R) {
+    return {} as { [T in keyof R]: T extends BaseOptionKeys ? BaseOptionRuleOutput[T] : R[T] }
 }
--- src/component/Rule/type/index.ts
+++ src/component/Rule/type/index.ts
@@ -1,24 +1,19 @@
-export interface RequiredOptions {
-    message?: string
-    tip?: string
+export interface BaseOptions {
+    required?:
+        | {
+              message?: string
+
+              tip?: string
+          }
+        | true
 }
 
-export interface Options {
-    required?: RequiredOptions | boolean
+export type BaseOptionKeys = keyof BaseOptions
 
-    /** 通过Option创建的自定义检验 */
-    /** @todo 添加类型 */
-    validator?: (val, formData, callback, props) => void | Promise<any>
+export type Validator = {
+    [key in string]?: (val: number, formData: string, callback, props) => void | Promise<void | Error>
 }
 
-export interface InnerRuleExecuteResult {
-    message?: (props: { type?: 'array' | 'string' }) => void | string
-    tip?: string
-    required?: boolean
-    validator?: (val, formData, callback, props) => void | Promise<any>
+export interface BaseOptionRuleOutput {
+    required: (message: string) => void | Promise<void | Error>
 }
-
-/** @todo 考虑将form传入 */
-export type RuleRender = (form) => Options
-
-export type RuleType = (Options | RuleRender)[]
