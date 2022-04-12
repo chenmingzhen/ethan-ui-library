a33abcd6cb5898c5f224fefc5ffcfc74577fdaff
chenmingzhen <214164051@qq.com>
2022-04-12 23:58:15 +0800

    remove strings util nocheck

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -1,12 +1,12 @@
-// @ts-nocheck
-export function capitalize(str) {
+export function capitalize(str: string) {
     if (typeof str !== 'string') {
         console.error(new Error('str should be a string'))
     }
+
     return str && str[0].toUpperCase() + str.slice(1)
 }
 
-export function substitute(str, obj) {
+export function substitute(str: string | ((obj: Record<string, any>) => any), obj: Record<string, any>) {
     if (typeof str === 'string') {
         if (str.indexOf('{') < 0) {
             return str
@@ -16,14 +16,17 @@ export function substitute(str, obj) {
             if (match.charAt(0) === '\\') {
                 return match.slice(1)
             }
+
             return obj[name] === null || obj[name] === undefined ? '' : obj[name]
         })
     }
     if (typeof str === 'function') {
         let val = str(obj)
+
         if (val === obj && typeof val === 'object') {
             val = Object.assign({}, obj)
         }
+
         return val
     }
 
