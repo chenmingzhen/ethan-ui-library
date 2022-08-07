7190cb40cbb7a32ec1ea2b0bf0196a0dc7f809aa
chenmingzhen <214164051@qq.com>
2022-08-07 23:58:34 +0800

    新增label文档

---
+++ site/pages/components/Form/example-09-labelWidth.tsx
@@ -0,0 +1,22 @@
+/**
+ * cn - 标签
+ *    -- 通过 labelWidth 和 labelAlign 改变标签宽度和对齐方式
+ * en - Label
+ *    -- Set labelWidth and labelAlign to change label with and alignment.
+ */
+import React from 'react'
+import { Form, Input } from 'ethan'
+
+export default function() {
+    return (
+        <Form labelWidth={200} labelAlign="right" style={{ maxWidth: 500 }}>
+            <Form.Item label="Email" name="email">
+                <Input />
+            </Form.Item>
+
+            <Form.Item label="Password" name="password">
+                <Input type="password" />
+            </Form.Item>
+        </Form>
+    )
+}
