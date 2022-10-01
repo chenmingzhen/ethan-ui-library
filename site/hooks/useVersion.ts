13986e04c7955e5e70824b12ffadfe1b90415d6a
chenmingzhen <214164051@qq.com>
2022-10-01 17:49:34 +0800

    调整version资源位置

--- site/hooks/useVersion.ts
+++ site/hooks/useVersion.ts
@@ -5,47 +5,40 @@ import locate from '../utils/locate'
 const verReg = /(\d+\.){2}x/
 
 const useVersion = () => {
-  const [versions, setVersions] = useState([])
+    const [versions, setVersions] = useState([])
 
-  const version = useMemo(() => {
-    const find = versions.find(v => window.location.pathname.indexOf(v.content) >= 0)
+    const version = useMemo(() => {
+        const find = versions.find(v => window.location.pathname.indexOf(v.content) >= 0)
 
-    if (find) return find.content
+        if (find) return find.content
 
-    return find
-  }, [versions])
+        return find
+    }, [versions])
 
-  const transformVersionUrl = useCallback((v, language) => {
-    const preUrl = window.location.href.split(verReg)[0]
+    const transformVersionUrl = useCallback((v, language) => {
+        const preUrl = window.location.href.split(verReg)[0]
 
-    return `${preUrl}${v}/${language}`
-  }, [])
+        return `${preUrl}${v}/${language}`
+    }, [])
 
-  useEffect(() => {
-    if (process.env.NODE_ENV === 'development') return
-    /**
-     * @deprecated  ethan-ui-pages/ 自己配置的目录下 与新增的images同级
-     * fetch('../../../versions.json')
-     */
+    useEffect(() => {
+        if (process.env.NODE_ENV === 'development') return
 
-    /**
-     * github pages
-     */
-    fetch('../../../versions.json')
-      .then(ver => ver.json())
-      .then(json => {
-        const language = locate('cn', 'en')
+        fetch('https://chenmingzhen.github.io/ethan-ui-library/versions.json')
+            .then(ver => ver.json())
+            .then(json => {
+                const language = locate('cn', 'en')
 
-        const jsonVersions = json.map(v => ({
-          content: v,
-          url: transformVersionUrl(v, language),
-        }))
+                const jsonVersions = json.map(v => ({
+                    content: v,
+                    url: transformVersionUrl(v, language),
+                }))
 
-        setVersions(jsonVersions)
-      })
-  }, [])
+                setVersions(jsonVersions)
+            })
+    }, [])
 
-  return [version, versions]
+    return [version, versions]
 }
 
 export default useVersion
