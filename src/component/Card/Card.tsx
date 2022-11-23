556d57df0b8b04f303bed00af9070bb3a6016546
chenmingzhen <214164051@qq.com>
2022-11-23 20:42:24 +0800

    移除Card submit

--- src/component/Card/Card.tsx
+++ src/component/Card/Card.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import classnames from 'classnames'
-import { dispatchEvent } from '@/utils/dom/element'
 import { cardClass } from '@/styles'
 import { compose } from '@/utils/func'
 import resizable from '@/hoc/resizable'
@@ -10,40 +9,21 @@ import { CardContext, CardProps } from './type'
 
 const Card: React.FC<CardProps> = ({ collapsible = false, forwardedRef, ...props }) => {
     const [collapsed, setCollapsed] = useState<boolean>(props.defaultCollapsed || true)
-    const [formStatus, setStatus] = useState<string>('')
-    const elRef = React.useRef<HTMLDivElement>()
 
-    // 判断是否为折叠状态
     const computedCollapsed = React.useMemo(() => {
         if (!collapsible) return undefined
 
-        // es2020特性
         return props.collapsed ?? collapsed
     }, [collapsed, props.collapsed, collapsible])
-
-    const setFormStatus = React.useCallback(
-        (status: string) => {
-            if (status !== formStatus) setStatus(status)
-        },
-        [formStatus]
-    )
 
     const handleCollapse = React.useCallback(() => {
         if (props.onCollapse) props.onCollapse(!computedCollapsed)
         else setCollapsed(!computedCollapsed)
     }, [computedCollapsed, props.onCollapse])
 
-    const handleSubmit = React.useCallback((target) => {
-        const form = elRef.current.querySelector('form')
-
-        if (form) dispatchEvent(form, 'submit', target)
-        else console.error(new Error('No form found.'))
-    }, [])
-
     const { shadow, className } = props
-    const newShadow = shadow === true ? 'shadow' : shadow
-    const newClasName = classnames(
-        cardClass('_', newShadow, collapsible && 'collapsible', collapsed && 'collapsed'),
+    const cls = classnames(
+        cardClass('_', shadow === true ? 'shadow' : shadow, collapsible && 'collapsible', collapsed && 'collapsed'),
         className
     )
 
@@ -51,13 +31,10 @@ const Card: React.FC<CardProps> = ({ collapsible = false, forwardedRef, ...props
         onCollapse: handleCollapse,
         collapsible,
         collapsed: computedCollapsed,
-        formStatus,
-        onSubmit: handleSubmit,
-        setFormStatus,
     }
 
     return (
-        <div className={newClasName} ref={forwardedRef} style={props.style}>
+        <div className={cls} ref={forwardedRef} style={props.style}>
             <Provider value={providerValue}>{props.children}</Provider>
         </div>
     )
