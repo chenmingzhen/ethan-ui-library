55be970d55927d30a66dbd7280de4adc2f6bf86c
chenmingzhen <214164051@qq.com>
2022-12-01 18:58:00 +0800

    完善Image类型

--- src/component/Image/index.ts
+++ src/component/Image/index.ts
@@ -1,14 +1,13 @@
-import { MemoExoticComponent, ForwardRefExoticComponent } from 'react'
 import Image from './Image'
 import Group from './Group'
 import { ImageProps } from './type'
 
-interface ImageComponent extends MemoExoticComponent<ForwardRefExoticComponent<ImageProps>> {
+interface ImageComponent extends React.ForwardRefExoticComponent<ImageProps & React.RefAttributes<HTMLDivElement>> {
     Group: typeof Group
     IS_ETHAN_IMAGE: boolean
 }
 
-const ComputedImage = Image as ImageComponent
+const ComputedImage = Image as unknown as ImageComponent
 
 ComputedImage.Group = Group
 ComputedImage.IS_ETHAN_IMAGE = true
