50954102318a7494c62eef10ac726ea68a56f67d
chenmingzhen <214164051@qq.com>
2023-01-05 13:24:26 +0800

    使用hooks重构Rate

--- src/component/Rate/Rate.tsx
+++ src/component/Rate/Rate.tsx
@@ -1,225 +1,176 @@
-import React from 'react'
-import classnames from 'classnames'
-import { range } from '@/utils/numbers'
-import { getParent } from '@/utils/dom/element'
+import useMergedValue from '@/hooks/useMergedValue'
 import { rateClass } from '@/styles'
-import { PureComponent } from '@/utils/component'
-import { RateProps, RateState } from './type'
+import { getParent } from '@/utils/dom/element'
+import { setTransformProp } from '@/utils/dom/translate'
+import { range } from '@/utils/numbers'
+import { styles } from '@/utils/style/styles'
+import classnames from 'classnames'
+import React, { useMemo, useRef, useState } from 'react'
+import { RateProps } from './type'
 
 const MIN_SIZE = 12
 
-export default class Rate extends PureComponent<RateProps, RateState> {
-    static defaultProps: RateProps = {
-        repeat: true,
-        max: 5,
-        size: 20,
-        text: [],
-        value: 0,
-    }
+const Rate: React.FC<RateProps> = function (props) {
+    const {
+        defaultValue,
+        onChange,
+        size = 20,
+        className,
+        style,
+        disabled,
+        max = 5,
+        allowHalf,
+        background,
+        repeat = true,
+        text = [],
+        front,
+        clearable,
+    } = props
+    const [hover, updateHover] = useState(0)
+    const [highlight, updateHighlight] = useState(0)
+    const [value, updateValue] = useMergedValue({
+        defaultStateValue: 0,
+        options: {
+            defaultValue,
+            value: props.value,
+            onChange(nextValue) {
+                if (onChange) {
+                    onChange(nextValue)
+                }
+            },
+        },
+    })
+    const highlightTimer = useRef<NodeJS.Timeout>()
+    const scale = size >= MIN_SIZE ? undefined : setTransformProp(`scale(${size / MIN_SIZE})`)
 
-    static displayName = 'EthanRate'
-
-    highlightTimer: NodeJS.Timeout
-
-    mouseInContainer = false
-
-    get scale() {
-        const { size } = this.props
-
-        if (size >= MIN_SIZE) return undefined
-
-        return {
-            transform: `scale(${size / MIN_SIZE})`,
-        }
-    }
-
-    get computedValue() {
-        const { value } = this.props
-
-        const { hover } = this.state
-
-        return hover === 0 ? value : hover
-    }
-
-    get InnerStyle() {
-        const { size } = this.props
-
+    const iconSizeStyle = useMemo<React.CSSProperties>(() => {
         if (!size) return undefined
 
         const parsed = Math.max(MIN_SIZE, size)
 
         return { width: parsed, fontSize: parsed }
+    }, [size])
+
+    function handleMouseLeave() {
+        updateHover(0)
     }
 
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            hover: 0,
-            highlight: 0,
-        }
-    }
-
-    handleRateClick = (value: number, e: React.MouseEvent<HTMLSpanElement>) => {
-        const { clearable, allowHalf, onChange } = this.props
-
+    function handleRateClick(nextValue: number, e: React.MouseEvent<HTMLSpanElement>) {
         if (allowHalf && getParent(e.target, `.${rateClass('allow-half')}`)) {
-            value -= 0.5
+            nextValue -= 0.5
         }
 
-        if (clearable && this.props.value === value) {
-            value = 0
-
-            this.setState({ hover: 0 })
+        if (clearable && nextValue === value) {
+            nextValue = 0
+            updateHover(0)
         }
 
-        onChange(value)
-
-        this.setState({ highlight: value })
+        updateValue(nextValue)
+        updateHighlight(nextValue)
 
         // 高亮动画
-        if (this.highlightTimer) {
-            clearTimeout(this.highlightTimer)
+        if (highlightTimer.current) {
+            clearTimeout(highlightTimer.current)
 
-            this.highlightTimer = null
+            highlightTimer.current = null
         }
 
-        this.highlightTimer = setTimeout(() => {
-            this.setState({ highlight: 0 })
+        highlightTimer.current = setTimeout(() => {
+            updateHighlight(0)
         }, 300)
     }
 
-    handleMove = (hover: number, e: React.MouseEvent) => {
+    function handleMove(nextHover: number, e: React.MouseEvent) {
         const { x, width } = (e.target as HTMLElement).getBoundingClientRect()
 
-        const value = hover - (x + width / 2 > e.clientX ? 0.5 : 0)
+        nextHover -= x + width / 2 > e.clientX ? 0.5 : 0
 
-        this.setState({ hover: value })
+        updateHover(nextHover)
     }
 
-    handleHover = (hover: number) => {
-        this.setState({ hover })
+    function handleHover(nextHover: number) {
+        updateHover(nextHover)
     }
 
-    handleMouseInContainer = () => {
-        this.mouseInContainer = true
-    }
-
-    handleMouseOutContainer = () => {
-        this.mouseInContainer = false
-
-        this.setState({ hover: 0 })
-    }
-
-    buildIcon = (icons: React.ReactNode | React.ReactNode[], i: number, isBg = false) => {
-        const { repeat, allowHalf } = this.props
-
-        const remain = this.computedValue - i
+    function buildIcon(icons: React.ReactNode | React.ReactNode[], i: number, isBg = false) {
+        const remain = shownValue - i
 
         let icon
 
         if (!Array.isArray(icons)) {
             icon = icons
         } else {
-            icon = icons[repeat ? this.computedValue - 1 : i]
+            icon = icons[repeat ? shownValue - 1 : i]
 
             if (!icon) icon = icons[icons.length - 1]
         }
 
         if (remain <= 0 || remain >= 1 || isBg) return icon
 
-        /** 半选remain是0.5 */
-        const style = { width: `${remain * 100}%`, display: 'block', overflow: 'hidden', fontSize: 'inherit' }
-
         /** 半选 */
         return (
-            <span style={style} className={allowHalf && rateClass('allow-half')}>
+            <span
+                style={{ width: `${remain * 100}%`, display: 'block', overflow: 'hidden', fontSize: 'inherit' }}
+                className={allowHalf && rateClass('allow-half')}
+            >
                 {icon}
             </span>
         )
     }
 
-    renderRate = () => {
-        const { front, max, text, allowHalf } = this.props
+    const shownValue = hover === 0 ? value : hover
+    const cls = classnames(rateClass('_', className))
+    const ms = styles(style, scale)
 
-        const { highlight } = this.state
-
-        return (
-            <div className={rateClass('front')}>
-                {range(max).map((v) => (
-                    <span
-                        key={v}
-                        onClick={this.handleRateClick.bind(this, v + 1)}
-                        onMouseMove={allowHalf ? this.handleMove.bind(this, v + 1) : undefined}
-                        onMouseEnter={!allowHalf ? this.handleHover.bind(this, v + 1) : undefined}
-                        style={this.InnerStyle}
-                    >
-                        {this.computedValue > v ? this.buildIcon(front, v) : <span>&nbsp;</span>}
-                        {/* 点击后的动画 */}
-                        {highlight === v + 1 && <i className={rateClass('highlight')}>{this.buildIcon(front, v)}</i>}
-                    </span>
-                ))}
-                <span className={rateClass('text')}>{text[Math.ceil(this.computedValue) - 1]}</span>
-            </div>
-        )
-    }
-
-    renderStatic = () => {
-        const { front, value, max, text } = this.props
-
-        return (
-            <div className={rateClass('static')}>
-                {range(max).map((v) => (
-                    <span key={v} style={this.InnerStyle}>
-                        {value > v && this.buildIcon(front, v)}
-                    </span>
-                ))}
-                <span className={rateClass('text')}>{text[Math.ceil(value) - 1]}</span>
-            </div>
-        )
-    }
-
-    renderBackground = () => {
-        const { background, max, disabled, allowHalf } = this.props
-
-        return (
+    return (
+        <div className={cls} style={ms} onMouseLeave={handleMouseLeave}>
             <div className={rateClass('background')}>
-                {range(max).map((v) => {
-                    const style = Object.assign(
-                        {
-                            visibility: !allowHalf && !disabled && this.computedValue > v ? 'hidden' : 'visible',
-                        },
-                        this.InnerStyle
-                    ) as React.CSSProperties
+                {range(max).map((index) => (
+                    <span
+                        key={index}
+                        style={styles(
+                            {
+                                visibility: !allowHalf && !disabled && shownValue > index ? 'hidden' : 'visible',
+                            },
+                            iconSizeStyle
+                        )}
+                    >
+                        {buildIcon(background, index, true)}
+                    </span>
+                ))}
+            </div>
 
-                    return (
-                        <span key={v} style={style}>
-                            {this.buildIcon(background, v, true)}
+            {disabled ? (
+                <div className={rateClass('static')}>
+                    {range(max).map((v) => (
+                        <span key={v} style={iconSizeStyle}>
+                            {value > v && buildIcon(front, v)}
                         </span>
-                    )
-                })}
-            </div>
-        )
-    }
-
-    render() {
-        const { disabled } = this.props
-
-        const className = classnames(rateClass('_', this.props.className))
-
-        const style = Object.assign({}, this.props.style, this.scale)
-
-        return (
-            <div
-                className={className}
-                style={style}
-                onMouseEnter={this.handleMouseInContainer}
-                onMouseLeave={this.handleMouseOutContainer}
-            >
-                {this.renderBackground()}
-
-                {disabled ? this.renderStatic() : this.renderRate()}
-            </div>
-        )
-    }
+                    ))}
+                    <span className={rateClass('text')}>{text[Math.ceil(value) - 1]}</span>
+                </div>
+            ) : (
+                <div className={rateClass('front')}>
+                    {range(max).map((v) => (
+                        <span
+                            key={v}
+                            onClick={handleRateClick.bind(this, v + 1)}
+                            onMouseMove={allowHalf ? handleMove.bind(this, v + 1) : undefined}
+                            onMouseEnter={!allowHalf ? handleHover.bind(this, v + 1) : undefined}
+                            style={iconSizeStyle}
+                        >
+                            {shownValue > v ? buildIcon(front, v) : <span>&nbsp;</span>}
+                            {/* 点击后的动画 */}
+                            {highlight === v + 1 && <i className={rateClass('highlight')}>{buildIcon(front, v)}</i>}
+                        </span>
+                    ))}
+                    <span className={rateClass('text')}>{text[Math.ceil(shownValue) - 1]}</span>
+                </div>
+            )}
+        </div>
+    )
 }
+
+Rate.displayName = 'EthanRate'
+
+export default React.memo(Rate)
--- src/component/Rate/index.tsx
+++ src/component/Rate/index.tsx
@@ -1,8 +1,3 @@
-import React from 'react'
 import Rate from './Rate'
-import withControl from '../../hoc/withControl'
-import { RateProps, RateState } from './type'
 
-const RateComponent = withControl(Rate) as React.ComponentClass<RateProps, RateState>
-
-export default RateComponent
+export default Rate
