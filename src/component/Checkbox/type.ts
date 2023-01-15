d2b6cf874f59a7326b6b2a8ee763e61c485fd980
chenmingzhen <214164051@qq.com>
2023-01-15 21:00:02 +0800

    删除无用的文件

--- src/component/Checkbox/type.ts
+++ src/component/Checkbox/type.ts
@@ -1,79 +1,48 @@
-import { KeyGen } from '@/type/Group'
-import List, { DatumListProps } from '@/utils/Datum/List'
 import React from 'react'
 
-export type CheckType = 'radio' | 'switch' | 'checkbox'
+type KeyGen<T> = T extends string | number ? true : keyof T | true | ((data: T) => string | number)
 
-export interface CheckHandlerContext<V = any> {
-    onGroupCallback?: (value: V, checked: boolean) => void
+type CheckBoxInferFormat<Data> = Data extends string | number ? never : keyof Data | ((data: Data) => string | number)
 
-    checked: (value: V) => boolean
-}
+type CheckboxInferRenderItem<Data> =
+    | (Data extends string | number ? React.ReactNode : keyof Data)
+    | ((item: Data) => React.ReactNode)
 
-export type Checked = boolean | 'indeterminate'
+export type CheckItemGroupBaseData = Record<string, any> | string | number
 
-export interface CheckItemProps<V = any> extends Omit<CheckHandlerContext<V>, 'checked'> {
-    checked?: Checked | CheckHandlerContext<V>['checked']
-    defaultChecked?: Checked
-    value?: V
-    onChange?: (checked?: boolean) => void
+export interface CheckboxProps {
+    checked?: boolean
+    defaultChecked?: boolean
+    value?: any
+    onChange?: (checked: boolean, index: number) => void
     onClick?: (e: React.MouseEvent<HTMLInputElement>) => void
-    size?: 'small' | 'default' | 'large'
-    /** Switch中用到 */
-    content?: React.ReactNode[]
     disabled?: boolean
     style?: React.CSSProperties
     className?: string
     children?: React.ReactNode
-}
-
-export interface ICheckedItemProps extends CheckItemProps {
     index?: number
-
-    internalOnChange?: (value, index: number) => void
+    indeterminate?: boolean
 }
 
-export interface CheckItemState {
-    checked: boolean
+export interface CheckboxGroupContext<V = any> {
+    onCheckboxGroupItemChange?: (value: V, checked: boolean) => void
+    checked: (value: V) => boolean
 }
-
-export type CheckboxProps<V = any> = Omit<CheckItemProps<V>, 'content' | 'size'>
-
-export type CheckItemGroupDefaultDataRecord = { label: React.ReactNode; value: string | number }
-
-export type CheckItemGroupBaseData = Record<string, any> | string | number
-
-export type CheckItemValue<Data, FormatData> = FormatData extends Data ? Data : FormatData
 
 export interface CheckboxGroupProps<
-    Data extends CheckItemGroupBaseData = CheckItemGroupDefaultDataRecord,
+    Data extends CheckItemGroupBaseData,
     FormatData extends CheckItemGroupBaseData = Data
-> extends Pick<DatumListProps<Data, FormatData>, 'format' | 'onChange' | 'prediction' | 'disabled'> {
-    /** 垂直布局 */
+> {
     block?: boolean
     data?: Data[]
-    renderItem?: keyof Data | ((item: Data) => React.ReactNode)
+    renderItem?: CheckboxInferRenderItem<Data>
     className?: string
     keygen?: KeyGen<Data>
     children?: React.ReactNode
-    defaultValue?: CheckItemValue<Data, FormatData>[]
-    value?: CheckItemValue<Data, FormatData>[]
-}
-
-/** FR显式说明format生成的结果类型 */
-export interface ICheckboxGroupProps<
-    Data extends CheckItemGroupBaseData = CheckItemGroupDefaultDataRecord,
-    FormatData extends CheckItemGroupBaseData = Data
-> extends CheckboxGroupProps<Data, FormatData> {
-    /** 设计缺陷:此处是内部使用必选的，不对外暴露 */
-    datum?: List<Data>
-}
-
-export interface CheckboxComponent<V = any> extends React.ComponentClass<CheckboxProps<V>> {
-    Group<
-        Data extends CheckItemGroupBaseData = CheckItemGroupDefaultDataRecord,
-        FormatResult extends CheckItemGroupBaseData = Data
-    >(
-        props: ICheckboxGroupProps<Data, FormatResult>
-    ): React.ReactElement<ICheckboxGroupProps<Data, FormatResult>, any>
+    defaultValue?: FormatData[]
+    value?: FormatData[]
+    format?: CheckBoxInferFormat<Data>
+    prediction?(formatValue: FormatData, data: Data): boolean
+    disabled?: boolean | ((data: Data) => boolean)
+    onChange?(value: FormatData[], data: Data, checked: boolean)
 }
