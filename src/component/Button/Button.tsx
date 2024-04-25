ee35468eb42a46b0a56cd25f4cba942b7574584c
chenmingzhen <214164051@qq.com>
2024-04-25 11:54:04 +0800

    默认值修改

--- src/component/Button/Button.tsx
+++ src/component/Button/Button.tsx
@@ -10,7 +10,7 @@ const Button: React.FC<ButtonProps> = (props) => {
 
     const {
         outline,
-        type = 'primary',
+        type = 'default',
         size,
         href,
         htmlType = 'button',
